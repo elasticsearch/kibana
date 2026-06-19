67273cda9f85145cb2500a9303dd68b590bbccd6
Maria Iriarte <maria.iriarte@elastic.co>
2026-06-19 15:26:52 +0200

    Migrate open in lens, agg based, metric test to scout

--- x-pack/platform/test/functional/apps/lens/open_in_lens/agg_based_1/index.ts
+++ x-pack/platform/test/functional/apps/lens/open_in_lens/agg_based_1/index.ts
@@ -70,6 +70,5 @@ export default function ({ loadTestFile, getService, getPageObjects }: FtrProvid
     });
 
     loadTestFile(require.resolve('./pie'));
-    loadTestFile(require.resolve('./metric'));
   });
 }
